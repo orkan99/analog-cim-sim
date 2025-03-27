8d818d2c47cd1ec741f75a6e81d41a72803a7941
Andrija Firez <firez@icelab-03.ice.rwth-aachen.de>
2025-03-27 15:50:32 +0100

    Still testing the FC layer

---
+++ rebuild_lib_debug.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get directory of script
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do 
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="${DIR}/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+rm -rf ${DIR}/build
+source ${DIR}/.venv/bin/activate
+mkdir -p ${DIR}/build/release/build && cd ${DIR}/build/release/build
+ 
+
+cmake \
+-DCMAKE_BUILD_TYPE=Debug \
+-DCMAKE_INSTALL_PREFIX=../ \
+-DPY_INSTALL_PATH=$DIR/.venv/lib/python3.11/site-packages \
+-DLIB_TESTS=OFF \
+-DBUILD_LIB_ACS_INT=ON \
+-DUSE_STDCXXFS=ON \
+../../../cpp
+
+make -j nproc
+make install 
+
+

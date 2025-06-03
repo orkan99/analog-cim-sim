f1329f49f444bfc864e06a4a41c9ee21d4767713
Andrija Firez <firez@icelab-03.ice.rwth-aachen.de>
2025-06-03 10:45:38 +0200

    Adapted build script and functions for working with analog cim sim to be useable with Brevitas

--- int-bindings/test/andrija_test.py
+++ int-bindings/test/andrija_test.py
@@ -6,8 +6,12 @@ import os
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 # indexing instuction (sheet, row - height, column - width)
 
+
+
+"""
 def unroll(array):
     return np.reshape(array, -1)
     
@@ -42,7 +46,7 @@ def extract_kernel_matrix(kernels):
         raise Exception("Something with dimensions is wrong, the input should be a 4D array which represents a list of 3D kernels!")
     return np.reshape(kernels, (np.shape(kernels)[0], -1))
 
-def calculate_ofm_matrix(ifm_matrix, kernel_matrix, cfg_file_path = "cpp/test/lib/configs/digital/I_DIFF_W_DIFF_1XB.json"):
+def calculate_ofm_matrix(ifm_matrix, kernel_matrix, cfg_file_path = "analog-cim-sim/cpp/test/lib/configs/digital/I_DIFF_W_DIFF_1XB.json"):
     with open(cfg_file_path, "r") as file:
             cfg = json.load(file)
     ofm_matrix = np.zeros((np.shape(kernel_matrix)[0], np.shape(ifm_matrix)[1]), dtype=np.int32)
@@ -76,7 +80,124 @@ def acs_conv2(ifm, kernels, padding = 0, stride = 1, dilation = 1):
     ofm = shape_ofm(ofm_matrix = ofm_matrix, out_h = out_h, out_w = out_w)
     return ofm
 
-def acs_fc(ifm, w_matrix, bias = None, cfg_file_path = "cpp/test/lib/configs/digital/I_DIFF_W_DIFF_1XB.json"):
+def acs_fc(ifm, w_matrix, bias = None, cfg_file_path = "analog-cim-sim/cpp/test/lib/configs/digital/I_DIFF_W_DIFF_1XB.json"):
+    with open(cfg_file_path, "r") as file:
+            cfg = json.load(file)
+    if(bias is None):
+        bias = np.zeros((1,np.shape(w_matrix)[0]))
+    else:
+        bias = np.reshape(bias, (1,-1))
+        if np.shape(bias)[-1] != np.shape(w_matrix)[0]:
+            if(len(np.shape())):
+                raise Exception("Bias dimension is not compatible with weights matrix and desired number of outputs!")
+    ofm_t = np.zeros((np.shape(w_matrix)[0], np.shape(ifm)[0]), dtype=np.int32)
+    ifm_t = np.transpose(ifm)
+    m_crossbar = cfg['M']
+    n_crossbar = cfg['N']
+    vg_num = int(np.ceil(np.shape(w_matrix)[1]/n_crossbar))
+    hg_num = int(np.ceil(np.shape(w_matrix)[0]/m_crossbar))
+    acs_int.set_config(os.path.abspath(cfg_file_path))
+    for  i in range(hg_num):
+        for j in range(vg_num):
+            cut = w_matrix[i*m_crossbar: (i + 1)*m_crossbar, j*n_crossbar: (j + 1)*n_crossbar]
+            m_matrix, n_matrix = np.shape(cut)
+            acs_int.cpy(np.reshape(cut, -1), m_matrix, n_matrix)
+            for k in range(np.shape(ifm_t)[1]):
+                result = np.zeros(m_matrix, dtype=np.int32)
+                acs_int.mvm(result, ifm_t[j*n_crossbar : (j+1)*n_crossbar, k], np.reshape(cut, -1), m_matrix, n_matrix)
+                ofm_t[i*m_crossbar : (i+1)*m_crossbar, k] += result
+    return np.transpose(ofm_t) + bias
+"""
+def unroll(array):
+    return np.reshape(array, -1)
+    
+def extract_ifm_column(ifm, pos_w : int, pos_h : int, kernel_size : tuple, dilation: int = 1):
+    #if(type(kernel_size) != tuple):
+      #  raise Exception("Kernel must be a tuple!")
+    if(kernel_size[1] % 2 == 0 or kernel_size[2] % 2 == 0):
+        raise Exception("Not implemented for even kernel size")
+    #column = ifm[:, pos_h - dilation*(kernel_size[1] - 1) // 2 : pos_h + dilation*(kernel_size[1] - 1) // 2 + 1 : dilation, pos_w - dilation*(kernel_size[2] - 1) // 2: pos_w + dilation*(kernel_size[2] - 1) // 2 + 1 : dilation] # was experimental for dilation, now works good
+    if(type(ifm) == torch.Tensor or type(ifm) == np.ndarray):
+        column = ifm[:, slice(pos_h - dilation*(kernel_size[1] - 1) // 2 , pos_h + dilation*(kernel_size[1] - 1) // 2 + 1 , dilation), slice(pos_w - dilation*(kernel_size[2] - 1) // 2, pos_w + dilation*(kernel_size[2] - 1) // 2 + 1 , dilation)] # the approach with slices works both for np arrays and tensors
+    else: 
+        column = ifm[0][:, slice(pos_h - dilation*(kernel_size[1] - 1) // 2 , pos_h + dilation*(kernel_size[1] - 1) // 2 + 1 , dilation), slice(pos_w - dilation*(kernel_size[2] - 1) // 2, pos_w + dilation*(kernel_size[2] - 1) // 2 + 1 , dilation)] # the approach with slices works both for np arrays and tensors
+    column = np.reshape(column, -1)
+    if(len(column) != math.prod(kernel_size)):
+        raise Exception("Kernel dimensions are different than the lenght of the extracted part of IMF (one column). Something is wrong!")
+    return column
+
+def extract_ifm_matrix(ifm, kernel_size: tuple, stride : int = 1, padding : int = 0, dilation : int = 1):
+    if(stride < 1 or padding < 0 or dilation < 1):
+        raise Exception("Something is wrong with passed parameters!")
+    out_w = int(np.floor( (np.shape(ifm)[2] + 2 * padding - dilation * (kernel_size[2] -1) - 1) / stride + 1 )) # based on Pytorch formulas for Conv2d from https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html#torch.nn.Conv2d
+    out_h = int(np.floor( (np.shape(ifm)[1] + 2 * padding - dilation * (kernel_size[1] -1) - 1) / stride + 1 ))
+    ifm_matrix = np.zeros((math.prod(kernel_size) , out_h*out_w))
+    if(type(ifm) == np.ndarray):
+        ifm = np.pad(ifm, ((0,0), (padding, padding), (padding, padding)))
+    else:
+        ifm = F.pad(input=ifm, pad=(padding, padding, padding, padding), mode='constant', value=0)
+    counter = 0
+    for i in range(out_w):
+        for j in range(out_h):
+            ifm_matrix[:, counter] = extract_ifm_column(ifm, pos_w = i*stride + dilation*(kernel_size[1]-1)//2, pos_h = j*stride + dilation*(kernel_size[2] - 1)//2, kernel_size=kernel_size, dilation = dilation) # works good for everything
+            counter += 1
+            
+    return ifm_matrix, out_h, out_w
+
+def extract_kernel_matrix(kernels):
+    if(len(np.shape(kernels)) != 4):
+        raise Exception("Something with dimensions is wrong, the input should be a 4D array which represents a list of 3D kernels!")
+    return np.reshape(kernels, (np.shape(kernels)[0], -1))
+
+def calculate_ofm_matrix(ifm_matrix, kernel_matrix, cfg_file_path = "analog-cim-sim/cpp/test/lib/configs/digital/I_DIFF_W_DIFF_1XB.json"):
+    with open(cfg_file_path, "r") as file:
+            cfg = json.load(file)
+    ofm_matrix = np.zeros((np.shape(kernel_matrix)[0], np.shape(ifm_matrix)[1]), dtype=np.int32)
+    m_crossbar = cfg['M']
+    n_crossbar = cfg['N']
+    vg_num = int(np.ceil(np.shape(kernel_matrix)[1]/n_crossbar))
+    hg_num = int(np.ceil(np.shape(kernel_matrix)[0]/m_crossbar))
+    acs_int.set_config(os.path.abspath(cfg_file_path))
+    for  i in range(hg_num):
+        for j in range(vg_num):
+            cut = np.array(kernel_matrix[i*m_crossbar: (i + 1)*m_crossbar, j*n_crossbar: (j + 1)*n_crossbar], dtype=np.int32)
+            m_matrix, n_matrix = np.shape(cut)
+            acs_int.cpy(np.reshape(cut, -1), m_matrix, n_matrix)
+            for k in range(np.shape(ifm_matrix)[1]):
+                result = np.zeros(m_matrix,dtype=np.int32)
+                acs_int.mvm(result, ifm_matrix[j*n_crossbar : (j+1)*n_crossbar, k], np.reshape(cut, -1), m_matrix, n_matrix)
+                res = np.matmul(cut, ifm_matrix[j*n_crossbar : (j+1)*n_crossbar, k])
+                if(not ((res - result) == 0).all()):
+                    print("A problem occured! ")
+                ofm_matrix[i*m_crossbar : (i+1)*m_crossbar, k] += result
+    return ofm_matrix
+
+def shape_ofm(ofm_matrix, out_w, out_h):
+    out_c = np.shape(ofm_matrix)[0]
+    out = np.ones((out_c, out_h, out_w))*-1
+    for i in range(out_c):
+        out[i][:][:] = np.reshape(ofm_matrix[i], (out_h, out_w), order='F')
+    return out
+
+def acs_conv2(ifm, kernels, padding = 0, stride = 1, dilation = 1, bias = None):    
+    kernel_matrix = extract_kernel_matrix(kernels = kernels)
+    
+    ofm_final = [] # number of images in a batch
+    for i in range(ifm.size()[0]):
+        if(type(ifm) == torch.Tensor or type(ifm) == np.ndarray):
+            ifm_matrix, out_h, out_w = extract_ifm_matrix(ifm = ifm[i], kernel_size=np.shape(kernels)[-3:], padding = padding, stride = stride, dilation = dilation)
+        else:
+            ifm_matrix, out_h, out_w = extract_ifm_matrix(ifm = ifm[0][i], kernel_size=np.shape(kernels)[-3:], padding = padding, stride = stride, dilation = dilation)
+        ofm_matrix = calculate_ofm_matrix(ifm_matrix = ifm_matrix, kernel_matrix= kernel_matrix)
+        ofm_final.append(torch.from_numpy(shape_ofm(ofm_matrix = ofm_matrix, out_h = out_h, out_w = out_w)).float())
+        
+
+    ofm_final = torch.stack(ofm_final)
+    if(bias is not None):
+        ofm_final += bias.view([1,kernels.size()[0],1,1])
+    return ofm_final
+
+def acs_fc(ifm, w_matrix, bias = None, cfg_file_path = "analog-cim-sim/cpp/test/lib/configs/digital/I_DIFF_W_DIFF_1XB.json"):
     with open(cfg_file_path, "r") as file:
             cfg = json.load(file)
     if(bias is None):
@@ -226,7 +347,8 @@ class Test_Conv2d(unittest.TestCase):
 
                     torch_conv2 = nn.functional.conv2d(torch.from_numpy(x), torch.from_numpy(kernels), padding=p, stride=s, dilation= 1).numpy()
                     np.testing.assert_array_equal(ofm, torch_conv2)
-                
+
+                                
 
 class Test_FC(unittest.TestCase):
 
--- rebuild_lib_release.sh
+++ rebuild_lib_release.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+##############################################################################
+# Copyright (C) 2025 Rebecca Pelke                                           #
+# All Rights Reserved                                                        #
+#                                                                            #
+# This is work is licensed under the terms described in the LICENSE file     #
+# found in the root directory of this source tree.                           #
+##############################################################################
 
 # Get directory of script
 SOURCE="${BASH_SOURCE[0]}"
@@ -8,22 +15,31 @@ while [ -h "$SOURCE" ]; do
     [[ $SOURCE != /* ]] && SOURCE="${DIR}/$SOURCE"
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+PROJ_DIR="$(cd -P "${DIR}/.." >/dev/null 2>&1 && pwd)"
 
 rm -rf ${DIR}/build
-source ${DIR}/.venv/bin/activate
+cd ${DIR}
 mkdir -p ${DIR}/build/release/build && cd ${DIR}/build/release/build
- 
+
+python_version=$(python3 --version 2>&1 | awk '{print $2}' | cut -d. -f1-2)
+echo ${python_version}
+
+LIB_INSTALL_PATH=${PROJ_DIR}/.venv/lib/python${python_version}/site-packages/pybind11/share/cmake/pybind11
 
 cmake \
--DCMAKE_BUILD_TYPE=Release \
--DCMAKE_INSTALL_PREFIX=../ \
--DPY_INSTALL_PATH=$DIR/.venv/lib/python3.11/site-packages \
--DLIB_TESTS=OFF \
--DBUILD_LIB_ACS_INT=ON \
--DUSE_STDCXXFS=ON \
-../../../cpp
+    -DCMAKE_C_COMPILER=gcc \
+    -DCMAKE_CXX_COMPILER=g++ \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPY_INSTALL_PATH=${PROJ_DIR}/.venv/lib/python${python_version}/site-packages \
+    -DCMAKE_PREFIX_PATH=${LIB_INSTALL_PATH} \
+    -DCMAKE_INSTALL_PREFIX=../ \
+    -DLIB_TESTS=ON \
+    -DBUILD_LIB_ACS_INT=ON \
+    -DBUILD_LIB_CB_EMU=ON \
+    -DUSE_STDCXXFS=ON \
+    ${PROJ_DIR}/analog-cim-sim/cpp
 
-make -j nproc
-make install 
-
+make -j `nproc`
+make install
+cd ${PROJ_DIR}/analog-cim-sim
 
